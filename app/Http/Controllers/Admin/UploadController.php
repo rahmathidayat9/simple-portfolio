e41955d6fbfc1328ab5cabba4b591eee3ce139de
Rahmat Hidayat <73263428+rahmathidayat9@users.noreply.github.com>
2022-10-21 23:52:30 +0700

    Update UploadController.php
    Fix Namespace Naming

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
